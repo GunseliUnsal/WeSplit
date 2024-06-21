864ecba00a58bf436044bd789c7d9f4850f43cc5
Günseli Ünsal <93467749+GunseliUnsal@users.noreply.github.com>
2024-06-21 12:36:14 +0300

    Update ContentView.swift

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -62,7 +62,7 @@ struct ContentView: View {
                 }
                 Section("Total Amount with tip"){
                     Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                }
+                }.foregroundStyle(tipPercentage == 0 ? .red : .primary)
                 
                 Section("Amount per person"){
                     Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
